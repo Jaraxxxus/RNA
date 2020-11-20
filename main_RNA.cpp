b0eccfc00948c8a23147e2450d16df1f82aad27c
Jaraxxxus <72243384+Jaraxxxus@users.noreply.github.com>
2020-11-20 11:26:46 +0700

    Update main_RNA.cpp

--- main_RNA.cpp
+++ main_RNA.cpp
@@ -1,46 +1,102 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
  typedef int tip;
 
 //===sizeof int: 4 bytes
 
 class RNA {
   public:	
-    int flag=0;
-	int *arr;
-	int cur;  //кол-во нуклидов обработано
-//	int curN=0; // позиция в слове
-//	tip curw=0;  // текущее слово, заполняетмя, по заполнению переносится в массив и обнуляется
-	int cursize=0;  //выделено байт
-	
+
     enum Nucleotide{
         T, C, G, A
     };
+ /*   int *arr;
+	int cursize;  //выделено байт
+	int cur;  //посл нуклеотид  
+	int flag;*/
+
+//----------------
+  private:
+    int *arr;
+	int cursize;  //выделено байт
+	int cur;  //посл нуклеотид  
+	int flag;   
+
+	
+    class reference {
+     private:
+        int pos;
+		int *pt;
+        
+
+ //---------------
+	 
+//--------------------------	 
+	    void setBit(int data){
+        	int mult=3 << 2*((sizeof(int)*4)-pos-1);
+   		    data= data<<2*((sizeof(int)*4)-pos-1);
+   	        mult =~mult;
+   	        *pt=(*pt& mult) | data;
+        }
+        int getBit() const{
+            int sdvig = 2*((sizeof(int)*4)-pos-1);
+            return ((*pt >> sdvig) & 0b00000011);
+        }
+     public:
+        reference(int Npos, int *Npt){
+            pos = Npos;
+            pt = Npt;            
+        }
+        Nucleotide operator!() const{
+            return (Nucleotide) (0b00000011 - getBit());
+        }
+        reference& operator=(int nucleotide){
+            setBit(nucleotide);
+            return *this;
+        }
+        reference& operator=(reference& ref){
+            setBit(ref.getBit());
+            return *this;
+        }
+        bool operator==(const reference& ref) const{
+            return getBit() == ref.getBit();
+        }
+        operator Nucleotide() const{
+            return (Nucleotide) getBit();
+        }
+        
+    };
+
+reference operator()(int pos) const{
+       
+        int i= pos % (4 * sizeof(int));
+        int j= pos / (4 * sizeof(int));
+        reference ref(i,&arr[j]);
+      return ref;
+ }
+//----------------
 
 
-
-
+public:
+    
 
 
 //==========выделяет память массиву   
   void setm (int size) {
-   cur= size;
-   size = (size/sizeof(int)/8+1)*2;
-   cout << "size our arr " << size;
-   cout << " " ;
-   arr = new int [size];   //в байтах, а не битах!!
-    for (int i = 0; i < size; i++) {
-        
-        arr[i] = 0;
-          
-    }
-    cursize= size;
-     flag++; 
+  	   if (!size) size=1;
+       cur= size;
+       size = (size/sizeof(int)/8+1)*2;
+       arr = new int [size];   //в байтах, а не битах!!
+       for (int i = 0; i < size; i++) { 
+        arr[i] = 0;         
+       }
+       cursize= size;
+       flag++; 
   }
   
-//==============увеличение размера массива в 2 раза
+//==============увеличение размера массива 
   void setNewm(int newsize){
         //int oldsize=size;
 		cur = newsize;  	
@@ -56,8 +112,6 @@ class RNA {
         arr = new_array;
         cursize= newsize;
         flag++;
-    
-		  
 		  
 }
 		  
@@ -65,6 +119,7 @@ class RNA {
 
  //===========получить   элемент  из массива 
    tip getn (int num) {
+   	if (num <=0 && num >= cursize) return 0;
    	int m=0; int n =0; tip z=0;int m1 = 0; int n1 = 0;
    	m = (num/(sizeof(tip)*4));  //элемент массива
    	
@@ -72,9 +127,6 @@ class RNA {
    	n = (num%(sizeof(tip)*4)); //элементе в массиве для поиска
    	n1 = (cur%(sizeof(tip)*4)); //колво нуклидов в последненм элементе массива
    	m1 = (cur/(sizeof(tip)*4));//последний элемент массива
-   	//if (n1==0) m1= m1-1;  //деление целочисленное, то работа в текущ байте
-   //	if (n==0) m= m-1;
- 
    
    	if (m == m1) {
    	
@@ -83,8 +135,7 @@ class RNA {
 	   return z & 0b00000011;
     }
    	else{
-   	
-   	//	cout << " n " << n;
+
    		z = arr[m];
     	z= z >> 2*((sizeof(tip)*4)-n-1);
     	return z & 0b00000011;}
@@ -95,45 +146,45 @@ class RNA {
    
 //============переопределение квардратных скобок================
 
-int operator [] (int pos) {
-if (cursize==0)
-{
-	 setm(pos);
-	 cur=pos;
-	 return 0;
-}
-else {
+//---------------------
+ reference& operator[](int pos) {
+        if ((pos / (4 * sizeof(int))) + 1 > cursize) {
+            if (!flag) setm(pos+1);
+			else setNewm(pos+1);
 
-   if (pos > cur) pos = cur;
-   return getn(pos);
-}
-
-};
-
+        }
+        if (pos < 0) pos=0;
+        if (pos > cur) cur = pos;
+        int i= pos % (4 * sizeof(int));
+        int j= pos / (4 * sizeof(int));
+        reference ref(i,&arr[j]);
+        return ref;
+    }
+/*
+const reference& operator[](int pos) const {
+       
+        
+        int i= pos % (4 * sizeof(int));
+        int j= pos / (4 * sizeof(int));
+        reference ref(i,&arr[j]);
+        return ref;
+    }*/
 
 
 //===========переопределение = создает копию RNA=============================
- RNA& operator=(const RNA rna) {
-  delete arr;
- 
-  arr = new tip[rna.cur];
-  cur = rna.cur;
-  cursize = rna.cursize;
+ RNA& operator =(const RNA rna) {
+    arr = new int[rna.cursize];
+    cur = rna.cur;
+    cursize = rna.cursize;
   
-  for (int i = 0; i < cursize; i++) {
-     arr[i] = rna.arr[i];
-  }
- return *this;
+    for (int i = 0; i < cursize; i++) {
+       arr[i] = rna.arr[i];
+    }
+    return *this;
  }
 
-//=============
-
-//===============
-
-
-
 //========удаляет хвост с номера=============
-   void delp (int num ) {
+ void delp (int num ) {
    int m=0; int n =0; tip z=0;int m1 = 0; int n1 = 0;
   	m = (num/(sizeof(tip)*4));  //элемент массива
    	n = (num%(sizeof(tip)*4)); //сдвиг в элементе
@@ -143,45 +194,29 @@ else {
    if (n1==0) m1= m1-1;  //деление целочисленное, то работа в текущ байте
    	if (n==0) m= m-1;
    	if (m==m1) {
-   	cout << " !!m " << m;
-   		 z = arr[m];
-   	   cout << " z " << z;
-	   cout << " sdvig " << 2*(n1-n);
+   		z = arr[m];
     	z= z >> 2*(n1-n);
 	    arr[m]=z;
 	   
     }
    	else{
-   	
-   		cout << " m " << m;
     	z = arr[m];
-     	cout << " z " << z;
-    	cout << " sdvig " << 2*((sizeof(tip)*4)-n-1);
     	z= z >> 2*((sizeof(tip)*4)-n-1);
  	    arr[m] = z;
    }
-   cout << " arr res " << arr[m];
    cur = num;
 }
 //=============== заменяет номер на введенный==================
   
-   void changeN (int num, int data ) {
+ void changeN (int num, int data ) {
    	//if (num == 0 || cur == 0) cur = 1; 
    	if (num > cur) cur=num;
-   	tip mult; 
+   	int mult; 
    	int m = (num/(sizeof(tip)*4));
    	int n = (num%(sizeof(tip)*4));
    	int n1 = (cur%(sizeof(tip)*4)); //колво нуклидов в последненм элементе
    	int m1 = (cur/(sizeof(tip)*4));//последний элемент массива
- /*  	cout << " m-" << m;
-   	cout << " n-" << n;
-   	cout << " nl-" << n1;
-   	cout << " ml-" << m1;
-   	cout << "" ;
-   	*/
-   	tip w = arr[m];
-  // 	cout << " word-" << w;
-  // 	cout << "" ;*/
+   	int w = arr[m];
    
    
     if (m ==m1) {
@@ -200,7 +235,6 @@ else {
    	w=w& mult;
    	w=w|data;
    	arr[m]=w;
-   //	cout << " res!!- " << arr[m];
    	
     }
 
@@ -220,10 +254,23 @@ bool operator!=(const RNA rna) const{
         return false;
 }
 
+
+
 RNA (int size) {
+	flag=0;
 	setm(size);
-	cur=size;
+
 }
+
+
+RNA() {
+ 
+  cursize = 0;
+  cur = 0;
+  flag = 0;
+
+}
+
 RNA (RNA&);
 
 
@@ -246,65 +293,55 @@ RNA (RNA&);
  }
 
 
-
 ~RNA(){
 	delete arr;	
 }
 
-};
 
 
+//--------------------
 
-   
-   
+//-----------------
+};   
    
    
 int main(int argc, char** argv) {
 	int i=0;
-	int n;
+	int n=3;
 	int ch;
-	tip c;
-	RNA BB(10), CC(10);
-	cout << " Dlina chepochki: ";
-	cin >> n;
+	int c;
+	RNA BB;
+//	cout << " Dlina chepochki: ";
+//	cin >> n;
 //	BB.setm (n );
-	 cout << "cepochka: " ;
-	 c = BB.arr[0];
-	 cout << c ;
-	 int z = n;
+//	 cout << "cepochka: " ;
+	 //c = BB.arr[0];
+	// cout << c ;
+    int z = n;
 	for (i =0; i < n; i++) {
 	
 	   cout << i+1 << " ) ";
 	   cin >> ch;
-	  // BB.readb(ch, n);
-	   BB.changeN(i, ch);
-	  /* cout << " slovo " << BB.arr[0];
-	   	cout << " nomer: ";
-	    cin >> i;
-	*/
-	
-	  //  c=   BB[i]; //BB.getn(i);
-	  //  cout << " find  " << c << " ";
+	   BB[i]= ch;
+	   cout << " BB[i]- " << BB[i];
 	   
        }
-       /* c = BB.arr[0];
-        cout << "resultat " ;
-	    cout << c ;*/
-     //   cout << " massiv " << BB.arr[0];
-   /* while (n>0){
-    	n--;
+
+ /*    z=BB.cur;
+    while (z>=0){
+    	z--;
     	
     	cout << " nomer: ";
-	    cin >> i;
+	    cin >> z;
 	
 	
-	 c=   BB[i]; //BB.getn(i);
+	 c=   BB[z]; //BB.getn(i);
 	  cout << " find  " << c;
     	
     	
-    }*/
+    }
 	
-	
+	*/
 	 
 /*     cout << " change nomer: ";
 	 cin >> i;
@@ -316,7 +353,7 @@ int main(int argc, char** argv) {
 	 cout << " change on value: ";
 	 cin >> c;
 	  BB.changeN(i, c);*/
-	  CC=BB;
+//	  CC=BB;
 //	  cout << " massiv2 " << CC.arr[0];
       int hh;
 	//  cout << " del with nomer: ";
@@ -326,7 +363,7 @@ int main(int argc, char** argv) {
 	  
 	//  CC+=BB;
 	 // cout << " massiv3 " << CC.arr[0];
-        BB.delp(3);
+//        BB.delp(3);
   /*       while (z >=0){
     	
     	
@@ -337,9 +374,10 @@ int main(int argc, char** argv) {
     	
     	z--;
     }*/
-    z=0;
-    //BB+=CC;
-    while (z <=10){
+        //BB+=CC;
+ /*   z=0;
+
+    while (z <=n){
     	
     	
     		
@@ -348,7 +386,7 @@ int main(int argc, char** argv) {
 	  cout << " find  " << c;
     	
     	z++;
-    }
+    }*/
     
 /*	if (BB!=CC) hh=1;
     cout << " eq " <<  hh;
